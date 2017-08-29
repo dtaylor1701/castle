6f9430dae87fd69cad882d2fc55cd5f457e9c43f
David Taylor <dktaylor1701@gmail.com>
2017-08-29 00:50:36 -0400

    added interaction

---
+++ Assets/Scripts/MainGame.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainGame : MonoBehaviour {
+
+    string[] dirList;
+    float xPos;
+
+    public GameObject door;
+	// Use this for initialization
+	void Start () {
+        dirList = getDirNum();
+        xPos = 0;
+
+        foreach(string path in dirList)
+        {
+            var doorN = (GameObject)Instantiate(door);
+            doorN.transform.position = new Vector3(xPos, 1, 0);
+            doorN.transform.localScale = new Vector3(1, 2, (float)0.1);
+            Text label = doorN.GetComponentInChildren<Text>();
+            label.text = path;
+            
+            xPos += 2;
+        }
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    string[] getDirNum()
+    {
+
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+        return Directory.GetDirectories(path);
+       
+    }
+}
